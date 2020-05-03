24830c11beaa1f1015c22fff1418b6ab25cc65af
nasakib <nat.sakib@gmail.com>
2020-05-03 08:22:04 -0400

    package-lock.json rm

---
+++ s.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const path = require("path");
+const compression = require("compression");
+
+const PORT = process.env.PORT ||  3000;
+
+const app = express();
+
+app.use(compression());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("/public/"));
+
+app.use(express.static(path.join(__dirname, "/build/")));
+app.get("/", function (req, res) {
+	res.sendFile(path.join(__dirname, "/build", "index.html"));
+});
+
+app.connect(process.env.PUBLIC_URL, {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
