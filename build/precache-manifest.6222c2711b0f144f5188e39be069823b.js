18c569603783f56d6c4d0c64e6974ef2feab6acd
nasakib <nat.sakib@gmail.com>
2020-05-03 09:05:16 -0400

    heroku stuff

---
+++ build/precache-manifest.6222c2711b0f144f5188e39be069823b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9fb0cb828ab816f12058bd75b1c33d2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4684d598386abb6ae5b9",
+    "url": "/static/css/main.d88feb00.chunk.css"
+  },
+  {
+    "revision": "e030589f94311154f849",
+    "url": "/static/js/2.a6110e5b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.a6110e5b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4684d598386abb6ae5b9",
+    "url": "/static/js/main.6b0ccdd1.chunk.js"
+  },
+  {
+    "revision": "30ef5604f756073ebd1e",
+    "url": "/static/js/runtime-main.23f53baa.js"
+  }
+]);
